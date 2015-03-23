0a905165534c4f9ca062d1e3928c8d7e2d8b9f22
nikom1337 <nikom1337@gmail.com>
2015-03-23 20:31:09 +0100

    helpdesk job and widget implementation

--- packages/default/jobs/helpdesk-job/helpdesk-job.js
+++ packages/default/jobs/helpdesk-job/helpdesk-job.js
@@ -4,24 +4,33 @@
  */
 
 module.exports = function(config, dependencies, job_callback) {
-
     var cvsParser = require('babyparse');
     var options = {
         url: config.cvsResource
     };
 
     dependencies.request(options, function (error, response, body) {
+        var tasks = [];
         if (!error && response.statusCode == 200) {
             var csv = body;
             var parsed = cvsParser.parse(csv);
-            console.log(parsed.data);
+
+            if (parsed.data != null && parsed.data.length > 0) {
+                for (var i = parsed.data.length - 1; i >= 0; i--) {
+                    if (parsed.data[i].length < 4) {
+                        continue;
+                    }
+
+                    tasks[i] = {
+                        title: parsed.data[i][1],
+                        type: parsed.data[i][2].toLowerCase(),
+                        author: parsed.data[i][3],
+                        project: parsed.data[i][4]
+                    }
+                };
+            }
         }
 
-        var task = {
-            type = "".toLowerCase(),
-            author = ""
-        }
-
-        job_callback(error, { title: "jakis tytul", text: "dupa dupa" });
+        job_callback(error, { title: "jakis tytul", tasks: tasks });
     });
 };
--- packages/default/widgets/helpdesk/helpdesk.js
+++ packages/default/widgets/helpdesk/helpdesk.js
@@ -1,13 +1,25 @@
 widget = {
-    //runs when we receive data from the job
-    onData: function(el, data) {
+	//runs when we receive data from the job
+	onData: function (el, data) {
+		var html = '<h2>HELPDESK</h2>';
+		
+		$.each(data.tasks, function (index, task) {
+			var styleCss = "error";
+			if (task.type == "awaria") {
+				styleCss = "failure";
+			}
 
-        //The parameters our job passed through are in the data object
-        //el is our widget element, so our actions should all be relative to that
-        if (data.title) {
-            $('h2', el).text(data.title);
-        }
+			html += '<div class="helpdesk helpdesk-' + styleCss + '">' 
+				+ '<div>' + task.title + '</div>'
+				+ ' <div class="helpdesk-info">by ' + task.author + ' - (' + task.project + ')</div>'
+			 + '</div>';
+		});
 
-        $('.content', el).html(data.text);
-    }
+		$('.content', el).html(html);
+	},
+	onError: function (el, data) {
+		var $error = $('<div class="container"><img src="images/warning.png"></div>');
+		$error.append($('<div class="error_message content"></span>').text(data.error));
+		$('.error', el).empty().append($error);
+	}
 };
